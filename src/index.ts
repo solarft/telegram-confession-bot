713968c097371de805cf243978ca379ac70cd4c3
Ahmad Ibrahim Shahmi <ahmadibrahim.shahmi@gmail.com>
2026-09-12 14:58:58 +0800

    Removed redis since it is not needed

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
-import { Redis } from '@upstash/redis'
 import { Bot } from 'grammy'
-import { BOT_TOKEN, UPSTASH_REDIS_TOKEN, UPSTASH_REDIS_URL } from './env'
+import { BOT_TOKEN } from './env'
 import * as commands from './handlers/commands'
 
 export const bot = new Bot(BOT_TOKEN)
-export const redis = new Redis({
-  url: UPSTASH_REDIS_URL,
-  token: UPSTASH_REDIS_TOKEN,
-})
-
 bot.command('start', commands.start)
