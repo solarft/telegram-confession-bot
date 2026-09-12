2af6e234cbb2565bf60b5a7dadc93089a3016a15
Ahmad Ibrahim Shahmi <ahmadibrahim.shahmi@gmail.com>
2026-09-12 21:05:06 +0800

    Changed the variable names

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Redis } from '@upstash/redis'
-import { UPSTASH_REDIS_TOKEN, UPSTASH_REDIS_URL } from './env'
+import { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } from './env'
 
 export { bot } from './bot'
 export const redis = new Redis({
-  url: UPSTASH_REDIS_URL,
-  token: UPSTASH_REDIS_TOKEN,
+  url: UPSTASH_REDIS_REST_URL,
+  token: UPSTASH_REDIS_REST_TOKEN,
 })
