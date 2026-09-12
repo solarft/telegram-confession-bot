edd7c5e94d363ca1b841e98c2268aa4a3d43b3b0
Ahmad Ibrahim Shahmi <ahmadibrahim.shahmi@gmail.com>
2026-09-12 23:23:33 +0800

    Registered the replyConfession conversation

--- src/bot.ts
+++ src/bot.ts
@@ -3,13 +3,18 @@ import { Bot, session } from 'grammy'
 import { BOT_TOKEN } from './env'
 import { registerAdminHandlers } from './handlers/admin'
 import * as commands from './handlers/commands'
-import { submitConfession, type BotContext } from './handlers/confession'
+import {
+  replyConfession,
+  submitConfession,
+  type BotContext,
+} from './handlers/confession'
 
 export const bot = new Bot<BotContext>(BOT_TOKEN)
 
 bot.use(session({ initial: () => ({}) }))
 bot.use(conversations())
 bot.use(createConversation(submitConfession, 'submitConfession'))
+bot.use(createConversation(replyConfession, 'replyConfession'))
 
 bot.command('start', commands.start)
 bot.hears('🗣️ New confession', (ctx) =>
