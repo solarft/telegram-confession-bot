d18be1c3964a06ad109ad3e956dc1fbd2a8b0666
Ahmad Ibrahim Shahmi <ahmadibrahim.shahmi@gmail.com>
2026-09-12 21:11:23 +0800

    Forgot to update some other variable

--- src/env.ts
+++ src/env.ts
@@ -4,8 +4,8 @@ export const TOKEN = process.env.TOKEN!
 export const BOT_TOKEN = process.env.BOT_TOKEN!
 export const CHANNEL_ID = process.env.CHANNEL_ID!
 export const ADMIN_GROUP_ID = process.env.ADMIN_GROUP_ID!
-export const UPSTASH_REDIS_REST_URL = process.env.UPSTASH_REDIS_URL!
-export const UPSTASH_REDIS_REST_TOKEN = process.env.UPSTASH_REDIS_TOKEN!
+export const UPSTASH_REDIS_REST_URL = process.env.UPSTASH_REDIS_REST_URL!
+export const UPSTASH_REDIS_REST_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN!
 
 if (!TOKEN || !BOT_TOKEN) {
   throw new Error('missing env')
